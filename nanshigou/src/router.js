dc967b36bf6a7312b08d03958cdc24ba32bf8145
yanfeng <1121346282@qq.com>
2019-05-05 11:00:03 +0800

    123

--- nanshigou/src/router.js
+++ nanshigou/src/router.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
 import HomePage from './routes/HomePage/HomePage'
+import ShoppingCar from './components/ShoppingCar/ShoppingCar'
+import Header from './components/Header/Header'
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
         <Route path="/home" exact component={HomePage} />
+        <Route path="/shop" exact component={ShoppingCar} />
+        <Route path="/head" exact component={Header} />
       </Switch>
     </Router>
   );
